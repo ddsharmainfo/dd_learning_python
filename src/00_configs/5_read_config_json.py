6968540bfc4c694228445230d3dae0c7bc4a302f
DD Sharma <ddsharmainfo@gmail.com>
2023-12-05 21:08:00 +0530

    updated configs

---
+++ src/00_configs/5_read_config_json.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+
+DIR = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(DIR, './5_config_json.json')
+ENV = 'Dev'
+
+
+def read_config(file_path, env):
+    with open(file_path, 'r') as file:
+        config = json.load(file)
+        key1 = config['key1']
+        key2 = config['key2']
+        key3 = config['key3']
+    return key1, key2, key3
+
+
+key1, key2, key3 = read_config(CONFIG_PATH, ENV)
+
+print('Value for Key1: ', key1)
+print('Value for Key2: ', key2)
+print('Value for Key3: ', key3)
