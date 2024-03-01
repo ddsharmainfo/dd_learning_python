941449cf1a52d563dfd5a0695b6671bf361b642d
DD Sharma <ddsharmainfo@gmail.com>
2024-03-01 15:31:58 +0530

    updated example

--- src/error_handling/basic.py
+++ src/error_handling/basic.py
@@ -15,10 +15,19 @@ try:
     result = divide(a, b)
 except ZeroDivisionError:
     # Handle specific exception
-    print("Error: Division by zero")
+    print("Error From Specific Exception: Division by zero")
+except FileNotFoundError as fnf_error:
+    # Handle specific exception
+    print('File Not Found Error: ', fnf_error)
+except RuntimeError as runtime_error:
+    # Handle specific exception
+    print('Run Time Error: ', runtime_error)
 except Exception as e:
     # Handle generic exception
     print("An error occurred:", e)
+else:
+    # No exception? Run this code
+    print('No exception? Run this code!')
 finally:
     # Finally block typically used for cleanup
     # run code which needs to execute regardless of error or not
